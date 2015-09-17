dff0dff7fa18fa6561e7200afd4ed738a2fcaa20
ewertonpetillo <ewerton.petillo@gmail.com>
2015-09-17 15:13:18 -0400

    Update

---
+++ Trab4/lista03/lista3.09/function.c
@@ -0,0 +1,48 @@
+
+/* Programacao de Computadores e ALgoritmos
+ * Trabalho 4
+ * function.c
+ *
+ * Equipe:
+ *
+ *          Paulo Henrique
+ *          Rai Santos
+ *          Jackson Kelvin
+ *          Wilson Calisto
+ *          Ewerton Petillo
+ *          
+ */
+#include<stdio.h>
+#define MAX 20
+
+int find_position(int a[][MAX], int x, int y)
+{
+    if (a[x][y] != -1 && ((a[x-1][y] == -1 && a[x+1][y] != -1) || (a[x][y-1] == -1 && a[x][y+1] != -1)))
+    {
+        return 1;
+    }else
+        return 0;
+}
+
+void print_matriz(int m[][MAX], int line, int col)
+{
+    int i, j;
+    for (i=0; i<line; i++)
+    {
+       for(j=0; j<col; j++)
+       {
+            printf("%d \t", m[i][j]);
+       }
+       printf("\n");
+    }
+}
+
+
+void write_matriz(int m[][MAX], int l, int c)
+{
+    int cont =0;
+
+    cont ++;
+    m[l][c] = cont;
+}
+
---
+++ Trab4/lista03/lista3.09/lista3.09.c
@@ -0,0 +1,50 @@
+
+/* Programacao de Computadores e ALgoritmos
+ * Trabalho 4
+ * lista3.9.c
+ *
+ * Equipe:
+ *
+ *          Paulo Henrique
+ *          Rai Santos
+ *          Jackson Kelvin
+ *          Wilson Calisto
+ *          Ewerton Petillo
+ *          
+ */
+
+#include <stdio.h>
+#include "function.c"
+#define MAX 20
+
+int main()
+{
+    int mat[MAX][MAX], line, col, i, j, cont;
+
+    printf("Quantas linhas? \n ");
+    scanf("%d", &line);
+    printf("Quantas colunas? \n ");
+    scanf("%d", &col);
+    for (i=0; i<line;i++)
+    {
+        for (j=0; j<col; j++)
+        {
+            printf("Digite o elemento[%d][%d]", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+    
+    for (i=0;i<line; i++)
+    {
+        for (j=0; j< col; j++)
+        {
+            if (find_position(mat, i, j) == 1)
+            {
+                write_matriz(mat, i, j);
+            }
+        }
+    }
+
+    print_matriz(mat, line, col);
+
+}
